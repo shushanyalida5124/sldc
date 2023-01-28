513557de2d5cf4d0d3b5a020f5bfcf50d9d66fa1
鸭梨 <785065948@qq.com>
2023-01-28 14:50:06 +0800

    Chore:修改版本号

--- src/node/__test__/transform.test.ts
+++ src/node/__test__/transform.test.ts
@@ -1,5 +1,5 @@
 import { describe, test, expect } from 'vitest';
-import transform from '../transform';
+import { transform } from '../transform';
 import { defineSLD } from '../../index';
 
 describe('transform', () => {
